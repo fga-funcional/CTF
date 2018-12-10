7e7f1cf1a48bc6566094e7a1848172fe21c0df79
Harrison Pedro <hpedro1195@gmail.com>
2018-12-10 18:47:26 -0200

    Add basic project runner

--- run.sh
+++ run.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
+cd backend/ctf/
+echo "main" | stack ghci --no-package-hiding &
+
+cd ../../front/
+elm reactor &
+
+cd ..
 devd -X /api/=http://localhost:3000 /=http://localhost:8000 -p 8080
